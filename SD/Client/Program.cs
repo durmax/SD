8a968337bd047206aebc8dccaa3bd7899603dfc2
dured.maksoud <dured.maksoud@gmail.com>
2020-05-29 14:29:55 +0200

    apiAzureUri

--- SD/Client/Program.cs
+++ SD/Client/Program.cs
@@ -18,7 +18,7 @@ namespace SD.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri("https://localhost:44394/") }); //new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri("https://sdapi20200529140234.azurewebsites.net/") }); //new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddMsalAuthentication(options =>
             {
                 var authentication = options.ProviderOptions.Authentication;

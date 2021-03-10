7642b5bbc7a70b03cd7ec46febc985b6daf64929
dured.maksoud <dured.maksoud@gmail.com>
2021-03-10 16:39:18 +0100

    improve Word

--- SD/Client/Pages/WordBase.cs
+++ SD/Client/Pages/WordBase.cs
@@ -132,6 +132,7 @@ namespace SD.Client.Pages
                         if (CurrUsrService.id == UserId)
                         {
                             await OnWordSave.InvokeAsync(wordModel);
+                            MyText = null;
                         }
                         await NewWordAsync();
                     }

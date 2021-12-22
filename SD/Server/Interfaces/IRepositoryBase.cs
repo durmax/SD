10b1efb81e470fd36c49008d3e053b43b6ef2c24
dmaksoud <dured.maksoud@adesso.de>
2021-12-22 17:31:39 +0100

    Add IRepositoryBase

---
+++ SD/Server/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace sd.Api.Interfaces
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<Task<T>> GetByCondation(Expression<Func<T,bool>> expression);
+        Task<bool> Create(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+    }
+}

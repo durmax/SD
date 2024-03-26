b53af69ff2760e73c9fbf254dd54988d6e3294f0
dmaksoud <dured.maksoud@adesso.de>
2024-03-26 14:52:02 +0100

    add required MongodbSettings props

--- SD/Server/Models/MongodbSettings.cs
+++ SD/Server/Models/MongodbSettings.cs
@@ -2,13 +2,13 @@
 {
     public class MongodbSettings : IMongodbSettings
     {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string UserCollectionName { get; set; }
-        public string WordCollectionName { get; set; }
-        public string OtherPageCollectionName { get; set; }
-        public string RelationshipCollectionName { get; set; }
-        public string UsersConfigsCollectionName { get; set; }
+        public required string ConnectionString { get; set; }
+        public required string DatabaseName { get; set; }
+        public required string UserCollectionName { get; set; }
+        public required string WordCollectionName { get; set; }
+        public required string OtherPageCollectionName { get; set; }
+        public required string RelationshipCollectionName { get; set; }
+        public required string UsersConfigsCollectionName { get; set; }
     }
 
     public interface IMongodbSettings

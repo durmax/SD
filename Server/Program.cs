049788112d83fb8d478634b51c924f0aa0e1320a
dured.maksoud@gmail.com <dured.maksoud@gmail.com>
2025-09-05 13:05:01 +0200

    API: Remove Startup file and add healthy check

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,20 +1,104 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+using sd.Api.Midlleware;
+using sd.Application;
+using sd.Infrastructure;
+using System.Collections.Generic;
 
-namespace sd.Api
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.Configure<JwtBearerOptions>(
+    JwtBearerDefaults.AuthenticationScheme, options =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        options.TokenValidationParameters.NameClaimType = "name";
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddDataProtection();
+
+builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddMemoryCache();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {
-                    webBuilder.UseStartup<Startup>();
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string>()
+                    }
                 });
-    }
+});
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+//app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseCors(builder =>
+{
+    builder.WithOrigins(
+        "https://localhost:44301",
+        "https://localhost:51473",
+        "https://lingoclub.netlify.app",
+        "https://www.lingoclub.net")
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .AllowCredentials();
+});
+
+app.UseAuthentication();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapHealthChecks("/healthz");
+
+await app.RunAsync();

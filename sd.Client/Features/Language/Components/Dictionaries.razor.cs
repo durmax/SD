efabb290fc56f1641f4ba77e82b658c4ea139177
dured.maksoud@gmail.com <dured.maksoud@gmail.com>
2026-05-12 15:11:02 +0200

    Dictionaries component

---
+++ sd.Client/Features/Language/Components/Dictionaries.razor.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using sd.Client.Contracts;
+using sd.Shared;
+using System.Collections.Generic;
+
+namespace sd.Client.Features.Language.Components
+{
+    public partial class Dictionaries
+    {
+        [Parameter] public List<DictionaryProviderDto> DictionaryProviders { get; set; } = new();
+        [Parameter] public IEnumerable<LanguageOption> SelectedItems { get; set; } = new List<LanguageOption>();
+        [Parameter] public LanguagePair ActivePair { get; set; } = default!;
+        [Parameter] public string FavSite { get; set; } = string.Empty;
+
+        [Parameter] public EventCallback<string> SetFavSiteAsync { get; set; }
+        [Parameter] public EventCallback<LanguagePair> PairChanged { get; set; }
+        [Parameter] public EventCallback Reverse { get; set; }
+        [Parameter] public EventCallback<FluentSortableListEventArgs> SortListAsync { get; set; }
+        [Parameter] public EventCallback<DictionaryProviderDto> RemoveItemAsync { get; set; }
+    }
+}
